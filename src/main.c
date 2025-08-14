4a238435969b42f7b78cd520935af23404faf8ac
John Duffy <johnkaneduffy@gmail.com>
2025-08-14 06:43:23 +0000

    add parser

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include "lex.h"
+#include "parse.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
 void remove_outer_quotes(char *str) {
 	size_t len = strlen(str);
 	if (len >= 2 && str[0] == '\'' && str[len - 1] == '\'') {
@@ -9,19 +12,54 @@ void remove_outer_quotes(char *str) {
         str[len - 2] = '\0'; 
     }
 }
+void parse_and_print(char* input) {
+    init_lexer(input, strlen(input));
+    Token* token = next_token();
+    SExp* sexp = parse_sexp(token);
+    print_sexp(sexp);
+    printf("\n");
+
+}
+
 int main(int argc, char **argv) {
   
-  char* input = argv[1];
-  remove_outer_quotes(input);
-  init_lexer(input, strlen(input));
-  //memory leak
-  Token *token = next_token();
-  while ((token->type != TOKEN_EOF)) {
-    print_token(token);
-    printf("\n");
-    free_token(token);
-    token = next_token();
-  }
+    int opt;
+    bool parse = false;
+	char* input = NULL;
+    while((opt = getopt(argc, argv, "pc:")) != -1) 
+    { 
+        switch(opt) 
+        { 
+			case 'p':
+				parse = true;
+				break;
+			case 'c':
+				input = optarg;
+				break;
+            case '?': 
+                printf("unknown option: %c\n", optopt);
+                break; 
+        } 
+    }
 
-  return 1;
+	if(!input) {
+		fprintf(stderr, "Usage: %s -c 'code'\n", argv[0]);
+		return 1;
+	}
+	remove_outer_quotes(input);
+	if(parse) {
+		parse_and_print(input);
+	} else {
+		init_lexer(input, strlen(input));
+		//memory leak
+		Token *token = next_token();
+		while ((token->type != TOKEN_EOF)) {
+			print_token(token);
+			printf("\n");
+			free_token(token);
+			token = next_token();
+		}
+	}
+
+  return 0;
 }

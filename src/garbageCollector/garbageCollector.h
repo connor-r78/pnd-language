81035fb0567a3626e93e0c4cdc814b496d47e858
connor-r78 <connorrakov@gmail.com>
2025-08-18 18:27:56 +0530

    Creating Pull Request For Creating Garbage Collector
    Create rudimentary garbage collector for pnd.
    
    To do:
    
    Improve garbage collection logic
    Ensure compilance with style guide

---
+++ src/garbageCollector/garbageCollector.h
@@ -0,0 +1,24 @@
+#ifndef GC_H
+#define GC_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void gc_init(void *stack_bottom_hint);
+
+void gc_register_root(void **ptr_location);
+
+void *gc_alloc(size_t size);
+
+void gc_collect(void);
+
+#define GC_NEW(T) ((T *)gc_alloc(sizeof(T)))
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

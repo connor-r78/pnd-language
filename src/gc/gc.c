11067cdfbd571f70fda7ff8a95ed7b68967d6a88
connor-r78 <connorrakov@gmail.com>
2025-08-31 19:36:40 -0400

    Replace Magic Numbers

--- src/gc/gc.c
+++ src/gc/gc.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MEMORY_ADDRESS_SIZE = 8
+#define MEMORY_ADDRESS_SIZE 8
+
+#define WHITE_SET_ID 0
+#define BLACK_SET_ID 1
+#define GREY_SET_ID 2
 
 struct GCObject {
   void* memAddress;
@@ -16,7 +20,7 @@ void gc_free(void** ptr) {
 }
 
 void push_gc_object(struct GCObject *object) {
-  int memAddressLength = sizeof(memAddress)/8;
+  int memAddressLength = sizeof(memAddress)/MEMORY_ADDRESS_SIZE;
 
   void* tempMemAddress[memAddressLength+1];
 
@@ -30,7 +34,7 @@ void push_gc_object(struct GCObject *object) {
 void* gc_alloc(size_t size) {
   int* ptr = malloc(size);
 
-  struct GCObject gcObject = {ptr, 0};
+  struct GCObject gcObject = {ptr, WHITE_SET_ID};
 
   push_gc_object(&gcObject);
   

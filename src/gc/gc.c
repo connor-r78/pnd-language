0f63ef8d4db219cbcca0d18fbe078184a90db088
connor-r78 <connorrakov@gmail.com>
2025-08-31 19:27:37 -0400

    Create push_gc_object()
    A function that pushes metadata of allocated memory to an array

--- src/gc/gc.c
+++ src/gc/gc.c
@@ -3,18 +3,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MEMORY_ADDRESS_SIZE = 8
+
 struct GCObject {
-  void* memAdd;
+  void* memAddress;
   int color;
 };
+
+void* memAddress[0];
 
 void gc_free(void** ptr) {
   free(*ptr);
   *ptr = NULL;
 }
 
+void push_gc_object(struct GCObject *object) {
+  int memAddressLength = sizeof(memAddress)/8;
+
+  void* tempMemAddress[memAddressLength+1];
+
+  for (int i = 0; i < memAddressLength; i++) {
+    tempMemAddress[i] = memAddress[i];
+  }
+
+  tempMemAddress[memAddressLength] = object->memAddress;
+}
+
 void* gc_alloc(size_t size) {
   int* ptr = malloc(size);
+
+  struct GCObject gcObject = {ptr, 0};
+
+  push_gc_object(&gcObject);
   
   return ptr;
 }
